5c3de91c7b4efe3966b9f15bac7d6e99d62d5798
lonema <393538042@qq.com>
2018-01-18 09:48:39 +0800

    add the SpringBootDemo+dubbo+zookeeper...

---
+++ SpringBoot+dubbo+zookepper/readme.txt
@@ -0,0 +1,7 @@
+该项目主要是用SpringBoot+Dubbo开发一个小demo，通过dubbo-admin来监控管理服务，zookeeper来注册
+dubbo:127.0.0.1:2181
+dubbo-admin: username:root password:root
+
+本地开发环境：
+	jdk1.7  tomcat7.0 
+	maven3.3.9 eclipse SpringBoot
---
+++ SpringBoot+dubbo+zookepper/springboot-dubbo-server/src/main/java/org/spring/springboot/ServerApplication.java
@@ -0,0 +1,28 @@
+package org.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+
+/**
+ * Spring Boot 应用启动类
+ *
+ * Created by bysocket on 16/4/26.
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+public class ServerApplication implements EmbeddedServletContainerCustomizer{
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(ServerApplication.class,args);
+    }
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8001);
+		container.setContextPath("/dubbo-server");
+	}
+}
